48476d94e626edb859435e13cb847dbb97d4dee3
Diego Torres <die.torres.gonzalez@gmail.com>
2021-04-29 20:43:44 -0700

    Agregar archivos de proyecto

---
+++ Torres_de_Hanoi/main.cpp
@@ -0,0 +1,12 @@
+/*****************************************************************//**
+ * \file   main.cpp
+ * \brief  Archivo principal del proyecto.
+ * 
+ * \date   29/04/2021
+ *********************************************************************/
+
+#include <iostream>
+
+int main() {
+	std::cout << "Hello World.\n";
+}
