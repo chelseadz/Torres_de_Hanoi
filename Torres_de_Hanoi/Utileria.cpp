eb122c9677b00b103647aadfd7e57ea8be00e606
Diego Torres <die.torres.gonzalez@gmail.com>
2021-05-03 17:15:12 -0700

    Agregar función para inicializar componentes de Allegro

--- Torres_de_Hanoi/Utileria.cpp
+++ Torres_de_Hanoi/Utileria.cpp
@@ -8,4 +8,13 @@
 
 #include "Utileria.h"
 
+#include <string>
 
+void initialize_al_component(bool flag, const char* comp_name) {
+	if (flag) return;
+
+	std::string except_msg("No se pudo inicializar ");
+	except_msg.append(comp_name);
+	
+	throw std::runtime_error(except_msg.append("."));
+}
