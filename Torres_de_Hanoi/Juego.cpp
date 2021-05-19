bd44f321190d5d1c775d2f45430fadac6365f0f3
ENRIQUE ALEJANDRO GIOTTONINI HERRERA <a220212781@unison.mx>
2021-05-19 16:18:14 -0700

    Crear una funcion para capturar el numero de discos incial desde otra pestania

--- Torres_de_Hanoi/Juego.cpp
+++ Torres_de_Hanoi/Juego.cpp
@@ -8,6 +8,91 @@
 
 #include "Juego.h"
 
-void Juego(ALLEGRO_EVENT_QUEUE* queue) {
 
+
+void Juego(ALLEGRO_EVENT_QUEUE* queue) {
+    int InitialDisks = 0;
+    InitialDisks = DiskNumber(queue);
+
+
+}
+
+int DiskNumber(ALLEGRO_EVENT_QUEUE* queue) {
+
+    ALLEGRO_FONT* font_title = al_load_font("ROBOTECH_GP.ttf", 48, 0);
+    ALLEGRO_FONT* font = al_load_font("ROBOTECH_GP.ttf", 36, 0);
+
+    initialize_al_component(font, "font");
+    initialize_al_component(font_title, "font titulo");
+
+    bool done = false;
+    bool redraw = true;
+    ALLEGRO_EVENT event;
+
+    int button_place = 0;
+    int Disks = 3;
+
+    while (1)
+    {
+        al_wait_for_event(queue, &event);
+
+        switch (event.type)
+        {
+        case ALLEGRO_EVENT_TIMER:
+            // nada por ahora.
+            redraw = true;
+            break;
+
+        case ALLEGRO_EVENT_KEY_DOWN:
+
+            if (event.keyboard.keycode == ALLEGRO_KEY_UP) {
+                button_place = _ADD;
+                if (button_place > MAX_DISKS)
+                    button_place = 0;
+                
+                if (Disks < MAX_DISKS)
+                    Disks++;
+            }
+
+            if (event.keyboard.keycode == ALLEGRO_KEY_DOWN) {
+
+                button_place = _SUBSTRACT;
+
+                if (button_place < 3)
+                    button_place = 0;
+
+                if (Disks > 3)
+                    Disks--;
+            }
+          
+            if (event.keyboard.keycode == ALLEGRO_KEY_SPACE)
+                done = true;
+
+            break;
+
+        case ALLEGRO_EVENT_DISPLAY_CLOSE:
+            done = true;
+            break;
+        }
+
+        if (done)
+            break;
+
+        if (redraw && al_is_event_queue_empty(queue))
+        {
+            NumberOfDisksDisplay(font_title, font, Disks);
+
+            ChangeDiskNumberDisplay(button_place);
+
+            al_flip_display();
+
+            redraw = false;
+        }
+    }
+
+    //Destruir fuentes creadas
+    al_destroy_font(font);
+    al_destroy_font(font_title);
+
+    return Disks;
 }
--- Torres_de_Hanoi/Juego.h
+++ Torres_de_Hanoi/Juego.h
@@ -13,8 +13,16 @@
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_ttf.h>
 #include <allegro5/allegro_primitives.h>
+#include <string>
 
 #include "Utileria.h"
+
+#define MAX_DISKS 7		// Maximo numero de discos en el juego.
+
+enum {
+    _ADD = 1,
+    _SUBSTRACT
+};
 
 /**
  * \brief Función principal del juego
@@ -23,6 +31,43 @@
  * \return void
  */
 void Juego(ALLEGRO_EVENT_QUEUE* queue);
+
+int DiskNumber(ALLEGRO_EVENT_QUEUE* queue);
+
+void NumberOfDisksDisplay(ALLEGRO_FONT* title, ALLEGRO_FONT* text, int Disks) {
+
+    //Screen
+    al_clear_to_color(al_map_rgb(3, 3, 100));
+    //Title
+    al_draw_text(title, al_map_rgb(255, 255, 255), _WINDOW_WIDTH / 2, _WINDOW_HEIGTH / 2, ALLEGRO_ALIGN_CENTER, "Numero de Discos: ");
+
+    //Disk Numbers
+    std::string tmp = std::to_string(Disks);
+    char const* num_char = tmp.c_str();
+
+    al_draw_filled_rectangle(_WINDOW_WIDTH / 5,  _WINDOW_HEIGTH / 9,  2 * _WINDOW_WIDTH / 5, 8 * _WINDOW_HEIGTH / 9, al_map_rgba_f(0, 0, 0.5, 0.3));
+    al_draw_text(text, al_map_rgb(255, 255, 255), 1.5 * _WINDOW_WIDTH / 5, 7 * _WINDOW_HEIGTH / 9, ALLEGRO_ALIGN_CENTER, num_char);
+
+    //Add Button
+    al_draw_filled_rectangle(_WINDOW_WIDTH / 5, 7 * _WINDOW_HEIGTH / 9, 2 * _WINDOW_WIDTH / 5, 8 * _WINDOW_HEIGTH / 9, al_map_rgba_f(0, 0, 0.5, 0.3));
+    //Substract Button
+    al_draw_filled_rectangle(3 * _WINDOW_WIDTH / 5, 7 * _WINDOW_HEIGTH / 9, 4 * _WINDOW_WIDTH / 5, 8 * _WINDOW_HEIGTH / 9, al_map_rgba_f(0, 0, 0.5, 0.5));
+
+}
+
+void ChangeDiskNumberDisplay(int Button) {
+
+    switch (Button)
+    {
+    case _ADD:
+        al_draw_filled_rectangle(_WINDOW_WIDTH / 5, 7 * _WINDOW_HEIGTH / 9, 2 * _WINDOW_WIDTH / 5, 8 * _WINDOW_HEIGTH / 9, al_map_rgba_f(0.5, 0.5, 0.5, 0.5));
+        break;
+    case _SUBSTRACT:
+        al_draw_filled_rectangle(3 * _WINDOW_WIDTH / 5, 7 * _WINDOW_HEIGTH / 9, 4 * _WINDOW_WIDTH / 5, 8 * _WINDOW_HEIGTH / 9, al_map_rgba_f(0.5, 0.5, 0.5, 0.5));
+        break;
+    }
+
+}
 
 
 #endif 
