9fdf0d43a96b3393b4dff0ab0a615c923b1ca9c2
DIEGO TORRES GONZALEZ <a220206322@unison.mx>
2021-05-21 20:05:27 -0700

    Cambiar mecánismo de selección por structuras de flecha que apuntan a una estaca.

--- Torres_de_Hanoi/Juego.cpp
+++ Torres_de_Hanoi/Juego.cpp
@@ -8,6 +8,7 @@
 
 #include "Juego.h"
 #include "Disco_y_Estaca.h"
+#include "Selector_flecha.h"
 
 #include <iostream>
 #include <stdexcept>
@@ -17,7 +18,7 @@
 
 enum EST_POS {
     INIT_X = 294,
-    Y_ESTS = 463 + 20,
+    Y_ESTS = 483,
     AUX_X = 600,
     FIN_X = 917
 };
@@ -28,7 +29,9 @@ enum {
     _FIN
 };
 
-#define _STICK_SIZE 318
+#define _STICK_SIZE 336
+
+#define _ARROW_SPACE 75
 
 #define _BASE_FILENAME "base_con_estacas.png"
 
@@ -56,16 +59,13 @@ void Juego(ALLEGRO_EVENT_QUEUE* queue, ALLEGRO_DISPLAY* display) {
 
     init.InitDiscsAndRods();
 
+    ALLEGRO_EVENT event;
 
     bool done = false;
     bool redraw = true;
-    ALLEGRO_EVENT event;
 
     bool move = false;
-    bool right = false;
     bool finish_movement = false;
-    bool selecting = true;
-    int Rod_Origin, Rod_Dest;
 
     ALLEGRO_BITMAP* base_and_stakes = al_load_bitmap(_BASE_FILENAME);
     try {
@@ -74,34 +74,97 @@ void Juego(ALLEGRO_EVENT_QUEUE* queue, ALLEGRO_DISPLAY* display) {
         std::cout << e.what() << '\n';
     }
 
+    Arrow_selector::Set_y_pos(EST_POS::Y_ESTS - _STICK_SIZE - _ARROW_SPACE);
+
+    Arrow_selector::Set_available_stakes(&init, &aux, &fin);
+
+    Arrow_selector origin(_LEFT_S);
+    Arrow_selector dest(_MIDDLE_S, 0);
+
+
     while (1)
     {
         al_wait_for_event(queue, &event);
 
         switch (event.type)
         {
+            case ALLEGRO_EVENT_TIMER:
 
-        case ALLEGRO_EVENT_TIMER:
+                if (move) {
+                    origin.pointee()->move_to_stake(dest.pointee(), move, finish_movement);
+                    if (move) {
+                        origin.show = false;
+                        dest.show = false;
+                    } else {
+                        origin.show = true;
+                        origin.selected = false;
+                        dest.selected = false;
+                        origin.selected_stake = _LEFT_S;
+
+                    }
+
+                }
 
 
+            case ALLEGRO_EVENT_KEY_DOWN: {
+                int key = event.keyboard.keycode;
 
-        case ALLEGRO_EVENT_KEY_DOWN:
-            if (event.keyboard.keycode == ALLEGRO_KEY_ESCAPE) {
-                if (move) finish_movement = true;
-                else done = true;
-            } else if (event.keyboard.keycode == ALLEGRO_KEY_RIGHT) {
-                move = true;
-                right = true;
-            } else if (event.keyboard.keycode == ALLEGRO_KEY_LEFT) {
-                move = true;
+                if (key == ALLEGRO_KEY_ESCAPE) {
+                    if (move) finish_movement = true;
+                    else if (origin.selected) {
+                        origin.selected = false;
+                        origin.show = true;
+                        dest.show = false;
+                    } else
+                        done = true;
+
+                }
+                else if (key == ALLEGRO_KEY_RIGHT) {
+                    if (!move) {
+                        if (!origin.selected) {
+                            origin.move_right();
+                        }
+                        else {
+                            dest.move_right();
+                            if (dest.selected_stake == origin.selected_stake)
+                                dest.move_right();
+                        }
+                    }
+                  
+
+                } else if (key == ALLEGRO_KEY_LEFT) {
+                    if (!move) {
+                        if (!origin.selected) {
+                            origin.move_left();
+                        }
+                        else {
+                            dest.move_left();
+                            if (dest.selected_stake == origin.selected_stake)
+                                dest.move_left();
+                        }
+                    }
+                }
+                else if (key == ALLEGRO_KEY_SPACE || key == ALLEGRO_KEY_ENTER) {
+
+                    if (!origin.selected) {
+                        origin.selected = true;
+                        if (origin.selected_stake == dest.selected_stake)
+                            dest.move_right();
+                        dest.show = true;
+                    } else {
+                        dest.selected = true;
+                        move = true;
+                    }
+                }
+
+                redraw = true;
+                break;
             }
-            redraw = true;
-            break;
 
-        case ALLEGRO_EVENT_DISPLAY_CLOSE:
-            done = true;
-            exit(0);
-            break;      
+            case ALLEGRO_EVENT_DISPLAY_CLOSE:
+                done = true;
+                exit(0);
+                break;      
         }
 
         if (done)
@@ -113,16 +176,18 @@ void Juego(ALLEGRO_EVENT_QUEUE* queue, ALLEGRO_DISPLAY* display) {
 
             al_draw_bitmap(base_and_stakes, 0, 0, 0);
 
-            init.PrintRodDiscs();
-            aux.PrintRodDiscs();
-            fin.PrintRodDiscs();
+            origin.draw();
+            dest.draw();
+            origin.pointee()->PrintRodDiscs();
+
+            if(origin.selected_stake != _LEFT_S) init.PrintRodDiscs();
+            if(origin.selected_stake != _MIDDLE_S) aux.PrintRodDiscs();
+            if(origin.selected_stake != _RIGTH_S) fin.PrintRodDiscs();
 
             al_flip_display();
 
             redraw = false;
         }
-
-
 
     }
 
@@ -250,178 +315,178 @@ int Min_n_moves(int n_discs) {
 
 
 
-int What_to_Move(ALLEGRO_EVENT_QUEUE* queue, Estaca &init, Estaca &aux, Estaca &fin) {
-
-    bool done = false;
-    bool redraw = true;
-    ALLEGRO_EVENT event;
-
-    int button_place = 0;
-
-    while (1)
-    {
-        al_wait_for_event(queue, &event);
-
-        switch (event.type)
-        {
-        case ALLEGRO_EVENT_TIMER:
-            // nada por ahora.
-            redraw = true;
-            break;
-
-        case ALLEGRO_EVENT_KEY_DOWN:
-
-            if (event.keyboard.keycode == ALLEGRO_KEY_RIGHT) {
-
-                if (button_place < _FIN)
-                    button_place++;
-            }
-            else if (event.keyboard.keycode == ALLEGRO_KEY_LEFT) {
-
-                if (button_place > _INIT)
-                    button_place--;
-            }
-            else if (event.keyboard.keycode == ALLEGRO_KEY_SPACE || event.keyboard.keycode == ALLEGRO_KEY_ENTER) {
-                return button_place;
-                done = true;
-            }
-                
-            
-
-            break;
-
-
-        case ALLEGRO_EVENT_DISPLAY_CLOSE:
-            done = true;
-            exit(0);
-            break;
-        }
-
-        if (done)
-            break;
-
-        if (redraw && al_is_event_queue_empty(queue))
-        {
-            al_clear_to_color(al_map_rgb(0, 0, 0));
-            init.PrintRodDiscs();
-            aux.PrintRodDiscs();
-            fin.PrintRodDiscs();
-            What_to_Move_Display(button_place);
-
-            al_flip_display();
-
-            redraw = false;
-        }
-    }
-}
-
-void What_to_Move_Display(int Button) {
-
-    switch (Button)
-    {
-    case _INIT:
-        al_draw_filled_triangle(EST_POS::INIT_X, 100, EST_POS::INIT_X - 30, 50 , EST_POS::INIT_X + 30, 50, al_map_rgba_f(239, 255, 0, 0.3));
-        break;
-    case _AUX:
-        al_draw_filled_triangle(EST_POS::AUX_X, 100, EST_POS::AUX_X - 30, 50, EST_POS::AUX_X + 30, 50, al_map_rgba_f(239, 255, 0, 0.3));
-        break;
-    case _FIN:
-        al_draw_filled_triangle(EST_POS::FIN_X, 100, EST_POS::FIN_X - 30, 50, EST_POS::FIN_X + 30, 50, al_map_rgba_f(239, 255, 0, 0.3));
-        break;
-
-    }
-}
-
-int Where_to_Move(ALLEGRO_EVENT_QUEUE* queue, int Origin, Estaca& init, Estaca& aux, Estaca& fin) {
-
-    bool done = false;
-    bool redraw = true;
-    ALLEGRO_EVENT event;
-
-    int button_place = Origin;
-
-    while (1)
-    {
-        al_wait_for_event(queue, &event);
-
-        switch (event.type)
-        {
-        case ALLEGRO_EVENT_TIMER:
-            // nada por ahora.
-            redraw = true;
-            break;
-
-        case ALLEGRO_EVENT_KEY_DOWN:
-
-            if (event.keyboard.keycode == ALLEGRO_KEY_RIGHT) {
-
-                if (button_place < _FIN)
-                    button_place++;
-            }
-            else if (event.keyboard.keycode == ALLEGRO_KEY_LEFT) {
-
-                if (button_place > _INIT)
-                    button_place--;
-            }
-            else if (event.keyboard.keycode == ALLEGRO_KEY_SPACE || event.keyboard.keycode == ALLEGRO_KEY_ENTER) {
-                return button_place;
-                done = true;
-            }
-
-            break;
-
-
-        case ALLEGRO_EVENT_DISPLAY_CLOSE:
-            done = true;
-            exit(0);
-            break;
-        }
-
-        if (done)
-            break;
-
-        if (redraw && al_is_event_queue_empty(queue))
-        {
-            al_clear_to_color(al_map_rgb(0, 0, 0));
-            init.PrintRodDiscs();
-            aux.PrintRodDiscs();
-            fin.PrintRodDiscs();
-            Where_to_Move_Display(Origin, button_place);
-
-            al_flip_display();
-
-            redraw = false;
-        }
-    }
-}
-
-void Where_to_Move_Display(int Origin, int Dest) {
-
-    switch (Origin)
-    {
-    case _INIT:
-        al_draw_filled_triangle(EST_POS::INIT_X, 100, EST_POS::INIT_X - 30, 50, EST_POS::INIT_X + 30, 50, al_map_rgba_f(239, 255, 0, 0.8));
-        break;
-    case _AUX:
-        al_draw_filled_triangle(EST_POS::AUX_X, 100, EST_POS::AUX_X - 30, 50, EST_POS::AUX_X + 30, 50, al_map_rgba_f(239, 255, 0, 0.8));
-        break;
-    case _FIN:
-        al_draw_filled_triangle(EST_POS::FIN_X, 100, EST_POS::FIN_X - 30, 50, EST_POS::FIN_X + 30, 50, al_map_rgba_f(239, 255, 0, 0.8));
-        break;
-
-    }
-
-    switch (Dest)
-    {
-    case _INIT:
-        al_draw_filled_triangle(EST_POS::INIT_X, 100, EST_POS::INIT_X - 30, 50, EST_POS::INIT_X + 30, 50, al_map_rgba_f(0.5, 0.5, 0.5, 0.5));
-        break;
-    case _AUX:
-        al_draw_filled_triangle(EST_POS::AUX_X, 100, EST_POS::AUX_X - 30, 50, EST_POS::AUX_X + 30, 50, al_map_rgba_f(0.5, 0.5, 0.5, 0.5));
-        break;
-    case _FIN:
-        al_draw_filled_triangle(EST_POS::FIN_X, 100, EST_POS::FIN_X - 30, 50, EST_POS::FIN_X + 30, 50, al_map_rgba_f(0.5, 0.5, 0.5, 0.5));
-        break;
-
-    }
-}
+//int What_to_Move(ALLEGRO_EVENT_QUEUE* queue, Estaca &init, Estaca &aux, Estaca &fin) {
+//
+//    bool done = false;
+//    bool redraw = true;
+//    ALLEGRO_EVENT event;
+//
+//    int button_place = 0;
+//
+//    while (1)
+//    {
+//        al_wait_for_event(queue, &event);
+//
+//        switch (event.type)
+//        {
+//        case ALLEGRO_EVENT_TIMER:
+//            // nada por ahora.
+//            redraw = true;
+//            break;
+//
+//        case ALLEGRO_EVENT_KEY_DOWN:
+//
+//            if (event.keyboard.keycode == ALLEGRO_KEY_RIGHT) {
+//
+//                if (button_place < _FIN)
+//                    button_place++;
+//            }
+//            else if (event.keyboard.keycode == ALLEGRO_KEY_LEFT) {
+//
+//                if (button_place > _INIT)
+//                    button_place--;
+//            }
+//            else if (event.keyboard.keycode == ALLEGRO_KEY_SPACE || event.keyboard.keycode == ALLEGRO_KEY_ENTER) {
+//                return button_place;
+//                done = true;
+//            }
+//                
+//            
+//
+//            break;
+//
+//
+//        case ALLEGRO_EVENT_DISPLAY_CLOSE:
+//            done = true;
+//            exit(0);
+//            break;
+//        }
+//
+//        if (done)
+//            break;
+//
+//        if (redraw && al_is_event_queue_empty(queue))
+//        {
+//            al_clear_to_color(al_map_rgb(0, 0, 0));
+//            init.PrintRodDiscs();
+//            aux.PrintRodDiscs();
+//            fin.PrintRodDiscs();
+//            What_to_Move_Display(button_place);
+//
+//            al_flip_display();
+//
+//            redraw = false;
+//        }
+//    }
+//}
+//
+//void What_to_Move_Display(int Button) {
+//
+//    switch (Button)
+//    {
+//    case _INIT:
+//        al_draw_filled_triangle(EST_POS::INIT_X, 100, EST_POS::INIT_X - 30, 50 , EST_POS::INIT_X + 30, 50, al_map_rgba_f(239, 255, 0, 0.3));
+//        break;
+//    case _AUX:
+//        al_draw_filled_triangle(EST_POS::AUX_X, 100, EST_POS::AUX_X - 30, 50, EST_POS::AUX_X + 30, 50, al_map_rgba_f(239, 255, 0, 0.3));
+//        break;
+//    case _FIN:
+//        al_draw_filled_triangle(EST_POS::FIN_X, 100, EST_POS::FIN_X - 30, 50, EST_POS::FIN_X + 30, 50, al_map_rgba_f(239, 255, 0, 0.3));
+//        break;
+//
+//    }
+//}
+//
+//int Where_to_Move(ALLEGRO_EVENT_QUEUE* queue, int Origin, Estaca& init, Estaca& aux, Estaca& fin) {
+//
+//    bool done = false;
+//    bool redraw = true;
+//    ALLEGRO_EVENT event;
+//
+//    int button_place = Origin;
+//
+//    while (1)
+//    {
+//        al_wait_for_event(queue, &event);
+//
+//        switch (event.type)
+//        {
+//        case ALLEGRO_EVENT_TIMER:
+//            // nada por ahora.
+//            redraw = true;
+//            break;
+//
+//        case ALLEGRO_EVENT_KEY_DOWN:
+//
+//            if (event.keyboard.keycode == ALLEGRO_KEY_RIGHT) {
+//
+//                if (button_place < _FIN)
+//                    button_place++;
+//            }
+//            else if (event.keyboard.keycode == ALLEGRO_KEY_LEFT) {
+//
+//                if (button_place > _INIT)
+//                    button_place--;
+//            }
+//            else if (event.keyboard.keycode == ALLEGRO_KEY_SPACE || event.keyboard.keycode == ALLEGRO_KEY_ENTER) {
+//                return button_place;
+//                done = true;
+//            }
+//
+//            break;
+//
+//
+//        case ALLEGRO_EVENT_DISPLAY_CLOSE:
+//            done = true;
+//            exit(0);
+//            break;
+//        }
+//
+//        if (done)
+//            break;
+//
+//        if (redraw && al_is_event_queue_empty(queue))
+//        {
+//            al_clear_to_color(al_map_rgb(0, 0, 0));
+//            init.PrintRodDiscs();
+//            aux.PrintRodDiscs();
+//            fin.PrintRodDiscs();
+//            Where_to_Move_Display(Origin, button_place);
+//
+//            al_flip_display();
+//
+//            redraw = false;
+//        }
+//    }
+//}
+//
+//void Where_to_Move_Display(int Origin, int Dest) {
+//
+//    switch (Origin)
+//    {
+//    case _INIT:
+//        al_draw_filled_triangle(EST_POS::INIT_X, 100, EST_POS::INIT_X - 30, 50, EST_POS::INIT_X + 30, 50, al_map_rgba_f(239, 255, 0, 0.8));
+//        break;
+//    case _AUX:
+//        al_draw_filled_triangle(EST_POS::AUX_X, 100, EST_POS::AUX_X - 30, 50, EST_POS::AUX_X + 30, 50, al_map_rgba_f(239, 255, 0, 0.8));
+//        break;
+//    case _FIN:
+//        al_draw_filled_triangle(EST_POS::FIN_X, 100, EST_POS::FIN_X - 30, 50, EST_POS::FIN_X + 30, 50, al_map_rgba_f(239, 255, 0, 0.8));
+//        break;
+//
+//    }
+//
+//    switch (Dest)
+//    {
+//    case _INIT:
+//        al_draw_filled_triangle(EST_POS::INIT_X, 100, EST_POS::INIT_X - 30, 50, EST_POS::INIT_X + 30, 50, al_map_rgba_f(0.5, 0.5, 0.5, 0.5));
+//        break;
+//    case _AUX:
+//        al_draw_filled_triangle(EST_POS::AUX_X, 100, EST_POS::AUX_X - 30, 50, EST_POS::AUX_X + 30, 50, al_map_rgba_f(0.5, 0.5, 0.5, 0.5));
+//        break;
+//    case _FIN:
+//        al_draw_filled_triangle(EST_POS::FIN_X, 100, EST_POS::FIN_X - 30, 50, EST_POS::FIN_X + 30, 50, al_map_rgba_f(0.5, 0.5, 0.5, 0.5));
+//        break;
+//
+//    }
+//}
---
+++ Torres_de_Hanoi/Selector_flecha.h
@@ -0,0 +1,102 @@
+/*****************************************************************//**
+ * \file   Selector_flecha.h
+ * \brief  Estructura de una flecha que el usuario usa para seleccionar estacas.
+ * 
+ * \author Equipo Rocket
+ * \date   21/05/2021
+ *********************************************************************/
+#pragma once
+#ifndef _SELECTOR_FLECHA_H
+#define _SELECTOR_FLECHA_H
+
+#include "Disco_y_Estaca.h"
+
+#include <climits>
+
+#define _ARROW_WIDTH 40
+#define _ARROW_HEIGTH 35
+
+#define _N_STAKES 3
+
+#define _NORMAL_COLOR YELLOW_RED
+#define _SELECTED_COLOR PALE_YELLOW_RED
+
+#define _NULL_SELECTION SHRT_MAX
+
+/**
+ * \brief Opciones que puede tomar variable selected_stake.
+ */
+enum {
+	_LEFT_S, /*< Estaca izquierda. */
+	_MIDDLE_S, /*< Estaca central. */
+	_RIGTH_S /*< Estaca derecha. */
+};
+
+struct Arrow_selector {
+	static float y_pos;
+	static Estaca* available_stakes[_N_STAKES];
+
+	short selected_stake = _NULL_SELECTION;
+	bool show;
+	bool selected = 0;
+
+
+
+	/**
+	 * \brief Constructor de objeto.
+	 * 
+	 * \param pointee Estaca a la que apunta la flecha.
+	 * \param show = 1 Si se tiene que mostrar la flecha o no.
+	 */
+	Arrow_selector(int selected_stake, bool show = 1);
+
+	/**
+	 * \brief Inicializa posición en y de centro para cada flecha.
+	 * 
+	 * \param y posición en y.
+	 */
+	static void Set_y_pos(float y);
+
+	/**
+	 * \brief Guarda las direcciones de las estacas para poder mover la flecha hacia ella cuando se pida.
+	 * 
+	 * \param left Estaca que se encuentra hasta la izquierda.
+	 * \param middle Estaca que se encuentra en el centro.
+	 * \param right Estaca que se encuentra a la derecha.
+	 */
+	static void Set_available_stakes(Estaca* left, Estaca* middle, Estaca* right);
+
+	/**
+	 * \brief Puntero a estaca a la cual está apuntando la flecha.
+	 * 
+	 * \return Puntero a estaca apuntada por la flecha.
+	 */
+	Estaca* pointee();
+
+	/**
+	 * \brief Hace que la flecha apunte a la flecha vecina derecha actual.
+	 * 
+	 * \details Si la flecha apunta a la estaca derecha,
+	 *  después de movimiento va a apuntar a la estaca izquierda.
+	 * 
+	 */
+	void move_right();
+
+	/**
+	 * \brief Hace que la flecha apunte a la flecha vecina derecha actual.
+	 * 
+	 * \details Si la flecha apunta a la estaca izquierda,
+	 *  después de movimiento va a apuntar a la estaca derecha.
+	 * 
+	 */
+	void move_left();
+	
+	/**
+	 * \brief Dibuja la flecha si la variable show es true.
+	 * 
+	 */
+	void draw();
+};
+
+
+#endif
